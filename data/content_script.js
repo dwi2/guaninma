565d9d9102c56d6bdf8f403815c9377630d7f86d
dwi2 <dwi019@gmail.com>
2015-03-31 00:25:53 +0800

    Update selector to fetch only latest revision

--- data/content_script.js
+++ data/content_script.js
@@ -6,10 +6,13 @@
   };
 
   var scan = function() {
-    var latestSelector = 'ng-view.ng-scope:ed';
-    var revision = document.querySelector('');
+    var revisionElem =
+      document.querySelector('ng-view.ng-scope > div:nth-child(2) span.revision-text');
+    var revision = revisionElem ? revisionElem.textContent : 'unknown';
+    emit('message', revision);
 
     var gijSelector =
+      'ng-view.ng-scope > div:nth-child(2) ' +
       'span.job-group[title="Gaia JS Integration Test"] + span.job-group-list' +
       ' > span.job-btn';
     var gijElements = document.querySelectorAll(gijSelector);
