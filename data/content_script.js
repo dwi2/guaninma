10ee05c2414dbe383ad4b04141c8da34d150de11
Sherman Chen <shamen.chens@gmail.com>
2015-03-31 11:58:15 +0800

    Add failed selectors

--- data/content_script.js
+++ data/content_script.js
@@ -6,17 +6,24 @@
   };
 
   var scan = function() {
+    var latestRevisionSection =
+      document.querySelector('ng-view.ng-scope > div:nth-child(2)');
     var revisionElem =
-      document.querySelector('ng-view.ng-scope > div:nth-child(2) span.revision-text');
+      latestRevisionSection.querySelector('span.revision-text');
     var revision = revisionElem ? revisionElem.textContent : 'unknown';
     emit('message', revision);
 
-    var gijSelector =
-      'ng-view.ng-scope > div:nth-child(2) ' +
-      'span.job-group[title="Gaia JS Integration Test"] + span.job-group-list' +
-      ' > span.job-btn';
-    var gijElements = document.querySelectorAll(gijSelector);
-    emit('message', 'got ' + gijElements.length + ' jobs');
+    var gijAllSelector = 'span.job-btn[title^="Gaia JS Integration Test"]';
+    var gijFailedSelector =
+      'span.job-btn.btn-orange[title^="Gaia JS Integration Test"] ' +
+      'span.job-btn.btn-purple[title^="Gaia JS Integration Test"] ' +
+      'span.job-btn.btn-black[title^="Gaia JS Integration Test"] ' +
+      'span.job-btn.btn-red[title^="Gaia JS Integration Test"] ';
+    var gijAllElements = latestRevisionSection.querySelectorAll(gijAllSelector);
+    var gijFailedElements = latestRevisionSection.querySelectorAll(gijFailedSelector);
+    emit('message',
+      'got ' + gijAllElements.length + ' jobs, ' +
+      'failed jobs: ' + gijFailedElements.length);
     // TODO:
   };
 
